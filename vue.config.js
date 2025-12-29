8e9f31d5e08cffc923e12fc8321a84d9901a9140
Matheus-C-Martins <50200318+Matheus-C-Martins@users.noreply.github.com>
2025-12-29 23:51:01 +0000

    fix: change transpileDependencies from boolean to array

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: [],
   publicPath: process.env.NODE_ENV === 'production' ? '/news/' : '/',
   devServer: {
     headers: {
